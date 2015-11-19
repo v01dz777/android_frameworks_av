3f82be5f54756f1dcc9715f62e586739c95e226b
Steve Kondik <steve@cyngn.com>
2015-11-18 16:42:19 -0800

    stagefright: Don't break the ABI in ACodec
     * Qualcomm's proprietary extension is doing some seriously bad stuff.
       This is not going to be fun to maintain.
    
    Change-Id: I7d9d709e7a1dfb3107a4c3ae7b2f15ddaa0fab74

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -2091,7 +2091,7 @@ status_t ACodec::configureCodec(
         } else {
             int32_t bitsPerSample = 16;
             msg->findInt32("bit-width", &bitsPerSample);
-            err = setupRawAudioFormat(
+            err = setupRawAudioFormatInternal(
                     encoder ? kPortIndexInput : kPortIndexOutput,
                     sampleRate,
                     numChannels, bitsPerSample);
@@ -2226,7 +2226,7 @@ status_t ACodec::configureCodec(
         } else {
             int32_t bitsPerSample = 16;
             msg->findInt32("bit-width", &bitsPerSample);
-            err = setupRawAudioFormat(kPortIndexInput, sampleRate, numChannels, bitsPerSample);
+            err = setupRawAudioFormatInternal(kPortIndexInput, sampleRate, numChannels, bitsPerSample);
         }
     } else if (!strncmp(mComponentName.c_str(), "OMX.google.", 11)
             && !strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_AC3)) {
@@ -2567,7 +2567,7 @@ status_t ACodec::setupAACCodec(
 
 status_t ACodec::setupAC3Codec(
         bool encoder, int32_t numChannels, int32_t sampleRate, int32_t bitsPerSample) {
-    status_t err = setupRawAudioFormat(
+    status_t err = setupRawAudioFormatInternal(
             encoder ? kPortIndexInput : kPortIndexOutput, sampleRate, numChannels, bitsPerSample);
 
     if (err != OK) {
@@ -2605,7 +2605,7 @@ status_t ACodec::setupAC3Codec(
 
 status_t ACodec::setupEAC3Codec(
         bool encoder, int32_t numChannels, int32_t sampleRate, int32_t bitsPerSample) {
-    status_t err = setupRawAudioFormat(
+    status_t err = setupRawAudioFormatInternal(
             encoder ? kPortIndexInput : kPortIndexOutput, sampleRate, numChannels, bitsPerSample);
 
     if (err != OK) {
@@ -2752,6 +2752,11 @@ status_t ACodec::setupFlacCodec(
 }
 
 status_t ACodec::setupRawAudioFormat(
+        OMX_U32 portIndex, int32_t sampleRate, int32_t numChannels) {
+    return setupRawAudioFormatInternal(portIndex, sampleRate, numChannels, 16);
+}
+
+status_t ACodec::setupRawAudioFormatInternal(
         OMX_U32 portIndex, int32_t sampleRate, int32_t numChannels, int32_t bitsPerSample) {
     OMX_PARAM_PORTDEFINITIONTYPE def;
     InitOMXParams(&def);
