46c1c1e856bebad4459ee025604ab7981ab20f06
Steve Kondik <steve@cyngn.com>
2016-04-22 18:32:39 -0700

    audiopolicy: Add AudioSessionInfo API
     * This patch introduces a new API which allows applications to
       query the state of the audio effects system, and receive
       callbacks with the necessary information to attach effects
       to any stream.
     * In the future, this may come as part of the AudioPort system,
       but since that's an active area of development by Google, we
       will dodge it for now.
     * The policy now simply keeps a refcounted list of objects which
       hold various bits of stream metadata. Callbacks are sent on
       stream open/close to applications which might be listening
       for them.
    
    Change-Id: I2d554d36e1378f4eb7b276010a3bfe8345c22ecd
    
    Conflicts:
    	include/media/AudioSession.h
    	services/audiopolicy/service/AudioPolicyEffects.cpp
    	services/audiopolicy/service/AudioPolicyEffects.h

--- services/audiopolicy/service/AudioPolicyEffects.h
+++ services/audiopolicy/service/AudioPolicyEffects.h
@@ -86,13 +86,13 @@ public:
 
     status_t updateOutputAudioSessionInfo(audio_io_handle_t output,
                              audio_stream_type_t stream,
-                             audio_session_t audioSession,
+                             int audioSession,
                              audio_output_flags_t flags,
                              audio_channel_mask_t channelMask, uid_t uid);
 
     status_t releaseOutputAudioSessionInfo(audio_io_handle_t output,
                              audio_stream_type_t stream,
-                             audio_session_t audioSession);
+                             int audioSession);
 
     status_t listAudioSessions(audio_stream_type_t streams,
                              Vector< sp<AudioSessionInfo>> &sessions);
@@ -205,7 +205,7 @@ private:
     // Automatic output effects are unique for audiosession ID
     KeyedVector< audio_session_t, EffectVector* > mOutputSessions;
     // Stream info for session events
-    KeyedVector< audio_session_t, sp<AudioSessionInfo> > mOutputAudioSessionInfo;
+    KeyedVector< int32_t, sp<AudioSessionInfo> > mOutputAudioSessionInfo;
 
     AudioPolicyService *mAudioPolicyService;
 };
